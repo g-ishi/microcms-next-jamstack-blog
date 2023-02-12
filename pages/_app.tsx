327740fb03bb85542c5ef52106d2007284674ae3
g-ishi <al14006@shibaura-it.ac.jp>
2023-02-12 11:46:38 +0900

    Add Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,35 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { googleTagManagerId } from '../utils/gtm';
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from '../components/GoogleTagManager';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* TODO: GoogleAnalyticsのシークレットを別ファイルにして管理する */}
+      {/* <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-PFQVZ1KBWR"
+      />
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || []
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date())
+            gtag('config', 'G-PFQVZ1KBWR', {
+              page_path: window.location.pathname,
+            })`,
+        }}
+      /> */}
+      <GoogleTagManager
+        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
